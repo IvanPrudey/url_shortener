621e0c1b552f0d78eb4cdd71fc61685c4f402c2e
IvanPrudey <prudey.ia@yandex.ru>
2025-10-02 21:23:48 +0300

    add_db_connect_function

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import os
+import sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+def get_db_connection():
+    connect = sqlite3.connect('database.db')
+    connect.row_factory = sqlite3.Row
+    return connect
 
 
 if __name__ == '__main__':
